4529c7d873cdcb158d034a4545919c24c89e20e1
hyderson <80370102+hyderson@users.noreply.github.com>
2024-02-26 14:56:15 +0800

    Update JavaScript_button.js

--- JavaScript_button/JavaScript_button.js
+++ JavaScript_button/JavaScript_button.js
@@ -4,4 +4,9 @@ const button = document.getElementById("toggle");
 button.onclick = function () {
     button.classList.toggle('dark');
     console.log("touch");
+    if(button.className==='dark'){
+        document.body.style.backgroundColor="black";
+    }else{
+        document.body.style.backgroundColor="antiquewhite";
+    }
 }
